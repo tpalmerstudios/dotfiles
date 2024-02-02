70b3167cfb72e9fff2c1e89a9de94be01d5e5890
Tim Palmer <tpalmerstudios@gmail.com>
2024-02-02 00:23:31 -0600

    changed natpmpc - fixes issue

--- natpmpc.zsh
+++ natpmpc.zsh
@@ -1,9 +1,8 @@
 #natpmpc -a 0 0 tcp 0
 while true
 do
-	date;
-	natpmpc -a 1 0 tcp 60 -g 10.96.0.1 &> /dev/null
-	natpmpc -a 1 0 udp 60 -g 10.96.0.1 &> /dev/null
+	natpmpc -a 1 54213 tcp 60 -g 10.96.0.1 &> /dev/null
+	natpmpc -a 1 54213 udp 60 -g 10.96.0.1 &> /dev/null
 	if [ $? -eq 0 ]
 
 	then
